a8d399579d2c2078068609d18a140107472056e7
kazmy <nehaluddin.dpe@gmail.com>
2023-12-16 21:41:43 +0530

    created basic functionality

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const app = express()
 const http = require('http').createServer(app)
 const socketio = require('socket.io')(http)
 
-const PORT = process.env.PORT || 3000
-
-
-http.listen(PORT, ()=> {
-    console.log(`server started ${PORT}`)
+http.listen(process.env.PORT || 3000, ()=> {
+    console.log(`server started ${process.env.PORT || 3000}`)
 })
 
 app.use(express.static(__dirname + '/src'))
@@ -17,7 +14,6 @@ app.use(express.static(__dirname + '/src'))
 app.get('/', (req,res) =>{
     res.sendFile(__dirname + '/index.html')
 })
-
 
 socketio.on('connection', (socket) =>{
     console.log('connected socket')
---
+++ src/client.js
@@ -0,0 +1,55 @@
+const socket = io()
+ 
+let name1;
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message_area')
+
+do{
+    name1 = prompt('please enter the name')
+    // console.log(name1)
+}while(!name1)
+
+textarea.addEventListener('keyup', (e) =>{
+   let a= e.code
+    // console.log(e.key +" nehal "+a)
+    if( e.key === "Enter"){
+        // console.log("enter in if")
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(msg1){
+    let msg ={
+        user : name1,
+        message :msg1.trim()
+    }
+    appendMessage(msg, "outgoing")
+    textarea.value = ""
+
+    scroll1()
+    socket.emit('message', msg)
+} 
+
+function appendMessage(msg,type){
+    let minDiv = document.createElement('div')
+    let className = type
+    minDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    minDiv.innerHTML = markup
+
+    messageArea.appendChild(minDiv)
+}
+
+socket.on('message', (msg) => {
+    // console.log(msg)
+    appendMessage(msg, "incoming")
+    scroll1()
+})
+
+function scroll1(){
+    messageArea.scrollTop = messageArea.scrollHeight
+}
